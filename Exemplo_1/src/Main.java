ffae80b4a37269d559c2a9eea8651fbd8afa0023
hugoresende27 <Hugoresende27@gmail.com>
2021-11-20 18:53:46 +0000

    Initial commit

---
+++ Exemplo_1/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá Hugo! Aprendendo Java!");
+    }
+}
+//classes em java sempre Maiúscula Camel case
+//psvm para criar metodo main
+//sout para print
+/////JDK = javac, development tools = JRE + Compiler
+///JRE = java runtime environement = JVM + Libraries
+//JVM = Java Virtual Machine
+
+//JVM (ENVIRONMENT) : carrega, verifica e executa o código; provide runtime environment; não existe fisicamente; independente de plataforma
+//JRE (RUN): Implementação do JVM; necessário para correr código JAVA; libraries usadas no JVM; existe fisicamente;  dependente de plataforma
+//JDK (RUN + PROGRAMMING): Kit completo de tools para desenvolvimento; contém JRE e Development tools
---
+++ Exemplo_1/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variáveis e tipos de dados
+        ///////////////////////////////////////////////////////
+        //numéricos////////////////////////////////////////////
+        byte b = 33;//1 byte
+        short nr = 3;//2 bytes
+        int age = 33;//4 bytes
+        float y = 15;//4 bytes //decimal
+        long x = 56;//8 bytes
+        double d = 5;//8 bytes //decimal
+        System.out.println(b+ nr+ age+ y +x+ d);
+        /////////////////////////////////////////////////////////
+        //texto/////////////////////////////////////////////////
+        char a = 67;
+
+    }
+}
