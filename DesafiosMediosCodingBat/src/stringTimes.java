1addaf9a995867d2ed9f26d6644a05290387a96e
hugoresende27 <Hugoresende27@gmail.com>
2021-11-30 21:23:32 +0000

    ter

---
+++ DesafiosMediosCodingBat/src/stringTimes.java
@@ -0,0 +1,20 @@
+/*
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi"
+ */
+public class stringTimes {
+    public static String stringTimes(String str, int n) {
+        if (n>=0){
+            return str.repeat(n);
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringTimes("Hi", 2));
+        System.out.println(stringTimes("Hi", 3));
+        System.out.println(stringTimes("Hi", 1));
+    }
+}
