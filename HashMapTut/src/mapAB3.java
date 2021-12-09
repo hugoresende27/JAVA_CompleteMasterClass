11c905651dd660c35349dbdb78c2332aa59299b5
hugoresende27 <Hugoresende27@gmail.com>
2021-12-09 12:49:32 +0000

    a

--- HashMapTut/src/mapAB3.java
+++ HashMapTut/src/mapAB3.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 import java.util.Map;
-
 /*
 Modify and return the given map as follows: if exactly one of the keys "a" or "b" has a value in the map (but not both),
 set the other to have that same value in the map.
@@ -9,14 +8,32 @@ mapAB3({"b": "bbb", "c": "cake"}) → {"a": "bbb", "b": "bbb", "c": "cake"}
 mapAB3({"a": "aaa", "b": "bbb", "c": "cake"}) → {"a": "aaa", "b": "bbb", "c": "cake"}
  */
 public class mapAB3 {
-    public Map<String, String> mapAB3(Map<String, String> map) {
-
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")==false){
+            map.put("b",map.get("a"));
+        }else if (map.containsKey("b") && map.containsKey("a")==false){
+            map.put("a",map.get("b"));
+        }
+        return map;
     }
 
     public static void main(String[] args) {
         Map<String,String> mp1 = new HashMap<>();
         mp1.put("a","aaa");
         mp1.put("c","cake");
+
+        Map<String,String> mp2 = new HashMap<>();
+        mp2.put("b","bbb");
+        mp2.put("c","cake");
+
+        Map<String,String> mp3 = new HashMap<>();
+        mp3.put("a","aaa");
+        mp3.put("b","bbb");
+        mp3.put("c","cake");
+
+        System.out.println(mapAB3(mp1));
+        System.out.println(mapAB3(mp2));
+        System.out.println(mapAB3(mp3));
 
     }
 }
